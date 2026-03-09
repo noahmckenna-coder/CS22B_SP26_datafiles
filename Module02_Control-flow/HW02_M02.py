6c35b7632c83b2832ebac067083187b517c599c6
NMcK123 <noah.mckenna@sjsu.edu>
2026-03-08 22:12:56 -0700

    Update HW02_M02.py

--- Module02_Control-flow/HW02_M02.py
+++ Module02_Control-flow/HW02_M02.py
@@ -1,5 +1,5 @@
 ### CS 22B Module 02 - Homework 2
-### Name: <Your Name>
+### Name: Noah McKenna
 
 ##### Homework practicing Control flow and Regular expression #####
 
@@ -24,43 +24,58 @@ lines = [
 
 ### step 1: Define the count variables
 error_count = re.compile(r"\bERROR\b.*\b(E\d{4})\b")
-#warn_count  = 
-#info_count  = 
+warn_count  = re.compile(r"\bWARN\b.*?\[([a-zA-Z0-9]+)\]")
+info_count  = re.compile(r"\bINFO\b.*user=([a-zA-Z0-9_]+)")
 
-### step 2: Initialize the count dictionary 
-#counts = {"ERROR": 0, ...}
+### step 2: Initialize the count dictionary
+counts = {"ERROR": 0, "WARN": 0, "INFO": 0, "SKIP": 0}
 
 ### step 3: Loop throught the dictionary with if-elif-else
-#for line in lines:
-#    if error_count.search(line):
-#        counts["ERROR"] += 1
-#    elif :
-#    else:
+for line in lines:
+    if error_count.search(line):
+        counts["ERROR"] += 1
+    elif warn_count.search(line):
+        counts["WARN"] += 1
+    elif info_count.search(line):
+        counts["INFO"] += 1
+    else:
+        counts["SKIP"] += 1
 
 print(counts)
 
-
+import re
 ##### Problem 2 - Check passwords with boolean logic and regex
 ## Write a function check_password(pw) that returns one of:
 # "STRONG" if:
 # - length ≥ 10
 # - contains at least 1 lowercase, 1 uppercase, 1 digit, 1 symbol from !@#$%^&*
 # - does not contain whitespace
-# otherwise "WEAK" 
+# otherwise "WEAK"
 ## Test the function on a list of passwords using a loop
 
 test_pw = ["Short7!", "LongerPass7!", "GoodPassw0rd!", "Bad Passw0rd!", "N0SymbolHereAAA"]
 
 ### step 1: Define variables
 lowercase = re.compile(r"[a-z]")
-#uppercase = 
-#digit_case = 
-#symbol_case = 
-#space_case = 
+uppercase = re.compile(r"[A-Z]")
+digit_case = re.compile(r"[0-9]")
+symbol_case = re.compile(r"[!@#$%^&*]")
+space_case = re.compile(r"[ ]")
 
 ### step 2: Write a function that check password (str) to test cases
-def check_password(<arg>):
-    strong = (...)
+def check_password(pw):
+    strong = (
+        len(pw) >= 10 and
+        bool(lowercase.search(pw)) and
+        bool(uppercase.search(pw)) and
+        bool(digit_case.search(pw)) and
+        bool(symbol_case.search(pw)) and
+        not bool(space_case.search(pw))
+    )
     return "STRONG" if strong else "WEAK"
 
 ### step 3: Use case. Test list test_pw
+for password in test_pw:
+    print(f'Password: "{password}" is {check_password(password)}')
+    strong = (...)
+    return "STRONG" if strong else "WEAK"
